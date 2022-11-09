501e9983d871f16d934278f25d005f1c35770bb0
dlsqja3561 <dlsqja3561@naver.com>
2022-11-09 16:19:34 +0900

    empList.jsp, Employee.java 추가

---
+++ src/main/java/vo/Employee.java
@@ -0,0 +1,10 @@
+package vo;
+
+public class Employee {
+	public int empNo;
+	public String birthdate;
+	public String firstName;
+	public String lastName;
+	public String gender;
+	public String hireDate;
+}
